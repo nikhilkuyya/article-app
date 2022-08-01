d24b147010497f71b725289e963bffe0a8d75759
nikhilkuyya <nikhilkuyya@gmail.com>
2022-08-01 20:16:39 +0530

    feat: user api for register and login

---
+++ api/src/resources/user/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.index({ email: 1 }, { unique: true });
+
+export const User = mongoose.model("user", UserSchema);
